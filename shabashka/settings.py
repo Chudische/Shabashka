1a43c1a196af0920a84acb64f19092f451d2f220
skpptf <62537172+skpptf@users.noreply.github.com>
2021-05-24 22:26:54 +0300

    Update settings.py

--- shabashka/settings.py
+++ shabashka/settings.py
@@ -24,6 +24,12 @@ dotenv_path = os.path.join(BASE_DIR, '.env')
 if os.path.exists(dotenv_path):    
     load_dotenv(dotenv_path)
 
+dotenv_path = '/srv/host.env'
+if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)    
+        
+HOSTING_NAME = os.getenv('HOSTING_NAME')
+HOSTING_NAME_S = os.getenv('HOSTING_NAME_S')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
