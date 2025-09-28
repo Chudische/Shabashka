98a40a41e4a5831d0438944ef86be6c351266a35
sakimov <chudis4e@gmail.com>
2025-09-28 17:19:20 -0300

    Translate all templates to English language
    add offer template to make code more reusable

--- main/templatetags/custom_tags.py
+++ main/templatetags/custom_tags.py
@@ -5,47 +5,6 @@ from ..models import Comment
 
 register = template.Library()
 
-@register.simple_tag()
-def time_measure(pk):
-    comment = Comment.objects.get(pk=pk)
-    time = comment.time_amount
-    if comment.measure == 'h':
-        if time == 1 or time % 10 == 1:
-            return "час"
-        elif time > 1 and time < 5 or time % 10 > 1 and time % 10 < 5:
-            return "часа"
-        elif time > 4 and time < 21 or time % 10 > 4 and time % 10 < 10:
-            return "часов"
-        else:
-            return 'ч'
-    if comment.measure == 'd':
-        if time == 1 or time % 10 == 1:
-            return "день"
-        elif time > 1 and time < 5 or time % 10 > 1 and time % 10 < 5:
-            return "дня"
-        elif time > 4 and time < 21 or time % 10 > 4 and time % 10 < 10:
-            return "дней"
-        else:
-            return 'д'
-    if comment.measure == 'w':
-        if time == 1 or time % 10 == 1:
-            return "неделя"
-        elif time > 1 and time < 5 or time % 10 > 1 and time % 10 < 5:
-            return "недели"
-        elif time > 4 and time < 21 or time % 10 > 4 and time % 10 < 10:
-            return "недель"
-        else:
-            return 'н'
-    if comment.measure == 'm':
-        if time == 1 or time % 10 == 1:
-            return "месяц"
-        elif time > 1 and time < 5 or time % 10 > 1 and time % 10 < 5:
-            return "месяца"
-        elif time > 4 and time < 21 or time % 10 > 4 and time % 10 < 10:
-            return "месяцов"
-        else:
-            return 'м'
-
 
 @register.simple_tag()
 def limit_text(content):
